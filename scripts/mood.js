a80ae015bbf8e30a05f2d2697c68df354a1f496e
Oribe Sofia <sofia.oribe@hotmail.com>
2022-12-09 14:26:46 -0300

    agrego comentario

--- scripts/mood.js
+++ scripts/mood.js
@@ -1,6 +1,7 @@
 const btnSwitch = document.querySelector("#switchId");
 
 btnSwitch.addEventListener("click", () => {
+    //Se agrega una clase al body
     document.body.classList.toggle("dark");
     btnSwitch.classList.toggle("active");
 });
